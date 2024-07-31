f0eec12e26ab6ddd98082ef1ad610d3b5c6e2354
sayada sudyindee <sayadasyd060650@gmail.com>
2024-07-31 14:05:03 +0700

    งานที่ 2 สัปดาห์ที่ 10

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { routes } from './../app.routes';
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonGrid, IonRow, IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [ RouterLink, IonButton, IonRow, IonGrid, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {
+  constructor() {}
+}
